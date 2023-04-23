c4d2bcc02e683a4bb9b0d6bd9b7c786846c85867
Ben Starr <bcstarr47@gmail.com>
2023-04-23 14:56:53 -0700

    Added Transitions

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -1,21 +1,52 @@
 package com.example.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var fabButton: FloatingActionButton
+    private lateinit var listLayout: ConstraintLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+//        return inflater.inflate(R.layout.fragment_list, container, false)
+
+
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        fabButton = view.findViewById(R.id.floatingActionButton)
+        fabButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        listLayout = view.findViewById(R.id.listLayout)
+        listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        // Set Menu
+        setHasOptionsMenu(true)
+
+
+        return view
+
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
 }
+
+
--- app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todoapp.R
 
 
@@ -15,7 +13,15 @@ class UpdateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        //Set Menu
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 }
